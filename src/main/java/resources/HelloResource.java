fe3f2f978fba8f0da1273a0eb4f1f0f7e50e78db
vinay3scale <vinay@3scale.net>
2014-07-18 02:54:10 +0530

    added param value

--- src/main/java/resources/HelloResource.java
+++ src/main/java/resources/HelloResource.java
@@ -30,6 +30,8 @@ public class HelloResource {
 
         ParameterMap usage = new ParameterMap(); // Add a metric to the call
         usage.add("hits", "1");
+        usage.add("configuration_v33", "1");
+
         params.add("usage", usage);              // metrics belong inside the usage parameter
 
         AuthorizeResponse response = null;
