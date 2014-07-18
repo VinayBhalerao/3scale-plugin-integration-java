582c5df1123a869c3fc5ba0dcec046d850908c50
vinay3scale <vinay@3scale.net>
2014-07-18 22:47:57 +0530

    added new key

--- src/main/java/resources/HelloResource.java
+++ src/main/java/resources/HelloResource.java
@@ -16,7 +16,7 @@ public class HelloResource {
 
     private String my_provider_key = "9062f7afd5a594f2e9882c34206eea67";
     //API Key
-    static String user_key            = "2a441e47aae6656ef244d3a6bfd298d0";
+    static String user_key            = "a8f9379565a1bd9597b25d52ba00d97c";
     static String user_key_service_id = "2555417722022";
 
 
