6ea5be6f25320f7eaaf25e25a78cb7bec271c201
vinay3scale <vinay@3scale.net>
2014-08-19 03:56:46 +0530

    added files

--- src/main/java/resources/HelloResource.java
+++ src/main/java/resources/HelloResource.java
@@ -10,13 +10,50 @@ import threescale.v3.api.ServerError;
 import threescale.v3.api.ServiceApi;
 import threescale.v3.api.impl.ServiceApiDriver;
 
+
 @Path("/hello")
 public class HelloResource {
 
-    @GET
-    @Produces("text/plan")
-    public String handleGreeting() {
-        return "Hello World of API's";
-    }
+    private String my_provider_key = "<your_provider_key>";
+    //API Key
+    static String user_key            = "<your_user_key>";
+    static String user_key_service_id = "<your_service_id>";
 
+
+    @GET
+    @Produces("text/plain")
+    public String handleGreeting() {
+        ServiceApi serviceApi = new ServiceApiDriver(my_provider_key);
+        ParameterMap params = new ParameterMap();              // the parameters of your call
+        params.add("user_key", user_key);               // Add the user key of your application for authorization
+        params.add("service_id", user_key_service_id);  // Add the service id of your application
+
+        ParameterMap usage = new ParameterMap(); // Add a metric to the call
+	usage.add("hits", "0");
+
+        params.add("usage", usage);              // metrics belong inside the usage parameter
+
+        AuthorizeResponse response = null;
+        // the 'preferred way' of calling the backend: authrep
+        try {
+          response = serviceApi.authrep(params);
+          String returnString1 = "AuthRep on User Key Success: " + response.success();
+          System.out.println(returnString1);
+          if (response.success() == true) {
+            // your api access got authorized and the  traffic added to 3scale backend
+            String returnString2 = "Plan: " + response.getPlan();
+            System.out.println(returnString2);
+            return "Hello World of API's";
+          } else {
+            // your api access did not authorized, check why
+            System.out.println("Error: " + response.getErrorCode());
+            System.out.println("Reason: " + response.getReason());
+            return "Error  " + response.getErrorCode() + response.getReason();
+          }
+        } catch (ServerError serverError) {
+          serverError.printStackTrace();
+        }
+        return "";
+    }
+    
 }
